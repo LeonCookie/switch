543ca122cc70df0e421517bda3ac6c8745a585c9
leonCookie <leongdynia1105@gmail.com>
2021-11-15 15:54:19 +0100

    private lateinit var

--- app/src/main/java/com/example/aswitch/MainActivity.kt
+++ app/src/main/java/com/example/aswitch/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.example.aswitch.databinding.ActivityMainBinding
 
@@ -15,6 +16,13 @@ class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
+    private  lateinit var imie:EditText
+    private  lateinit var nazw:EditText
+    private  lateinit var mail:EditText
+    private  lateinit var wiek:EditText
+    private  lateinit var phone:EditText
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +37,39 @@ class MainActivity : AppCompatActivity() {
         private fun setCheckedChangeListener() {
 
 
+            imie = findViewById<EditText>(R.id.editTextImie)
+            nazw = findViewById<EditText>(R.id.editTextImie)
+            mail = findViewById<EditText>(R.id.editTextImie)
+            wiek = findViewById<EditText>(R.id.editTextImie)
+            phone = findViewById<EditText>(R.id.editTextImie)
 
             binding.switchImie.setOnCheckedChangeListener { _, isChecked ->
 
                 if(binding.switchImie.isEnabled){
+                    imie
+                }
+            }
+            binding.switchNazw.setOnCheckedChangeListener { _, isChecked ->
+
+                if(binding.switchNazw.isEnabled){
+
+                }
+            }
+            binding.switchImie.setOnCheckedChangeListener { _, isChecked ->
+
+                if(binding.switchMail.isEnabled){
+
+                }
+            }
+            binding.switchImie.setOnCheckedChangeListener { _, isChecked ->
+
+                if(binding.switchWiek.isEnabled){
+
+                }
+            }
+            binding.switchImie.setOnCheckedChangeListener { _, isChecked ->
+
+                if(binding.switchPhone.isEnabled){
 
                 }
             }
